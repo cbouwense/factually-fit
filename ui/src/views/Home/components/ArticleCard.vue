367798b65374034d8ef31cf12a09b6f78bb0f8f7
Christian Bouwense <bouwensec@gmail.com>
2021-05-01 10:50:18 -0400

    some styling for different devices

--- ui/src/views/Home/components/ArticleCard.vue
+++ ui/src/views/Home/components/ArticleCard.vue
@@ -17,14 +17,14 @@ export default {
 </script>
 
 <style scoped>
-/* Mobile Vertical */
+/* Mobile Portrait */
 * {
   color: white;
   justify-content: space-around;
 }
 
 a {
-  background-image: linear-gradient(to bottom right, orange, purple);
+  background-image: linear-gradient(to bottom right, cyan, purple);
   border-radius: 5px;
   box-shadow: 2px 2px 4px 4px #ccc;
   display: flex;
@@ -35,7 +35,7 @@ a {
 }
 
 h1 {
-  font-size: 22px;
+  font-size: 30px;
   font-weight: 700;
   margin-bottom: 15px;
   word-wrap: break-word;
@@ -46,29 +46,61 @@ h2 {
   font-weight: 400;
 }
 
-/* Mobile Horizontal */
-@media screen and (min-width: 640px) {
+/* Mobile Landscape */
+@media screen 
+  and (min-device-width: 360px)
+  and (orientation: landscape) {
   a {
     min-height: 33vh;
   }
   
   h1 {
-    font-size: 36px;
+    font-size: 40px;
   }
 
   h2 {
-    font-size: 22px;
+    font-size: 24px;
   }
 }
 
 /* Tablet Vertical */
-@media screen and (min-width: 768px) {
+@media screen 
+  and (min-device-width: 768px)
+  and (orientation: portrait) {
+  a {
+    min-height: 20vh;
+  }
   
+  h1 {
+    font-size: 50px;
+  }
+
+  h2 {
+    font-size: 24px;
+  }
 }
 
 /* Tablet Horizontal */
+@media screen 
+  and (min-device-width: 768px)
+  and (orientation: landscape) {
+  a {
+    min-height: 33vh;
+  }
+  
+  h1 {
+    font-size: 60px;
+  }
+
+  h2 {
+    font-size: 30px;
+  }
+}
 
 /* Laptop */
+@media screen and (min-device-width: 1366px) {
+  
+}
 
 /* Desktop */
 
