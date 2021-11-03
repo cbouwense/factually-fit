cc7d2999cfbeb0360f68ef92196e971b9b86a5d5
Christian Bouwense <bouwensec@gmail.com>
2021-11-02 20:29:55 -0400

    got the MVP title and search bar in place.

---
+++ ui/src/components/Search.svelte
@@ -0,0 +1,7 @@
+<div class="container">
+  <input class="input is-success is-large" type="text" placeholder="Try &quot;keto&quot;">
+</div>
+
+<style>
+  @import "https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css";
+</style>
--- ui/src/routes/index.svelte
+++ ui/src/routes/index.svelte
@@ -1,11 +1,17 @@
-<h1>Factually Fit</h1>
-<!-- Search -->
+<script>
+  import Search from "../components/Search.svelte";
+</script>
+
+<div class="container">
+  <div class="columns is-centered">
+    <div class="column is-half">
+      <h1 class="title is-1 has-text-centered my-6">Factually Fit</h1>
+      <Search />
+    </div>
+  </div>
+</div>
 <!-- Article List -->
 
 <style>
-  h1 {
-    font-size: 100px;
-    margin: 100px auto 0 auto;
-    text-align: center;
-  }
+  @import "https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css";
 </style>
