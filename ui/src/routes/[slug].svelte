102d1456f4936c5d19d9a9ebe4d828604cb685ab
Christian Bouwense <bouwensec@gmail.com>
2021-11-11 14:25:20 -0500

    firgured out how to get stuff in the onMount to work without needing the endpoint business.

--- ui/src/routes/[slug].svelte
+++ ui/src/routes/[slug].svelte
@@ -1,21 +1,25 @@
-<script context="module">
-	/**
-	 * @type {import('@sveltejs/kit').Load}
-	 */
-	export function load({ page }) {
-    return {
-			props: { 
-        articleId: page.params.slug 
-      }
-		};
-	}
-</script>
-
 <script>
-  export let articleId = "";
+  import { getStores /*, page */ } from '$app/stores';
+  import { onMount } from "svelte";
+
+  let article = {}; // TODO: type this
+  let isLoaded = false;
+  const { page } = getStores();
+
+  onMount(async () => {
+    try {
+      console.log("page: ", page);
+      console.log("$page: ", $page);
+      const res = await fetch(`http://localhost:3001${$page.path}`); // TODO: extract this to an interface or something.
+      article = await res.json();
+    } catch (e) {
+      console.error(e);
+    }
+    isLoaded = true;
+  });
 </script>
 
-<h1 class="title">{articleId}</h1>
+<h1 class="title">articleId</h1>
 
 <style>
   @import "https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css";
