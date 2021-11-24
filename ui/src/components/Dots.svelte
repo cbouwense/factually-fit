82606d6ada0becdb8bc166bbc652522e97082b83
Christian Bouwense <bouwensec@gmail.com>
2021-11-24 17:00:22 -0500

    added Dots component.

---
+++ ui/src/components/Dots.svelte
@@ -0,0 +1,9 @@
+<p class="has-text-centered is-size-3">...</p>
+
+<style>
+  @import "https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css";
+  
+  p {
+    letter-spacing: 15px;
+  }
+</style>
