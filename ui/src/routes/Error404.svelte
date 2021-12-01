3833454d9f139dfe0b842aef5e065d04b05793a1
Christian Bouwense <bouwensec@gmail.com>
2021-12-01 18:04:22 -0500

    used on:click to be more svelte-like.

--- ui/src/routes/Error404.svelte
+++ ui/src/routes/Error404.svelte
@@ -2,7 +2,7 @@
   <div class="container">
     <div class="content is-medium">
       <h1>Oops! We couldn't find that page.</h1>
-      <button class="button is-link is-light" onclick="location.href = '/'">Article List</button>
+      <button class="button is-link is-light" on:click={() => { location.href = "/" }}>Article List</button>
     </div>
   </div>
 </div>
