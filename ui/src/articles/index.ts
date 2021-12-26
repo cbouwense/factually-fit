79b3ecbc93ae2452b555ef6eed621416dea0c456
Christian Bouwense <bouwensec@gmail.com>
2021-12-26 18:38:11 -0500

    finished second draft of article about food quality.

--- ui/src/articles/index.ts
+++ ui/src/articles/index.ts
@@ -1,7 +1,12 @@
+import FoodQuality from "./FoodQuality";
 import GlycogenAndWaterWeight from "./GlycogenAndWaterWeight";
 
 // TODO: is there a programmatic way of doing this?
 export default {
+  "food-quality": {
+    gist: FoodQuality.gist,
+    long: FoodQuality.long,
+  },
   "glycogen-and-water-weight": {
     gist: GlycogenAndWaterWeight.gist,
     long: GlycogenAndWaterWeight.long,
