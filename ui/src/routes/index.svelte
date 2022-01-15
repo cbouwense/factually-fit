66af8e1e66ea1ecc67b0f45113bf05009396bdae
Christian Bouwense <bouwensec@gmail.com>
2022-01-15 16:15:38 -0500

    fixed css loading issue.

--- ui/src/routes/index.svelte
+++ ui/src/routes/index.svelte
@@ -7,7 +7,7 @@
   <div class="columns is-centered">
     <div class="column is-half">
       <figure class="image is-4by1">
-        <img src="/logo-original-1000.png" alt="logo" />
+        <img src="/logo.png" alt="logo" />
       </figure>
       <Search />
       <ArticleList />
@@ -15,9 +15,7 @@
   </div>
 </div>
 
-<style>
-  @import "https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css";
-  
+<style>  
   img {
     object-fit: cover;
     height: 250px;
